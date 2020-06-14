9e6c6152bb07f257466fb4313206991601714641
sdas1991 <sourangshu_das_tict@yahoo.co.in>
2020-06-14 12:14:14 -0700

    changes for navigation button

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,15 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>helloooo</Text>;
+const HomeScreen = (props) => {
+  return (<View>
+    <Text style={styles.text}>helloooo</Text>
+    <Button onPress = {()=>props.navigation.navigate('Components')}
+    title = "button element" />
+    <TouchableOpacity onPress={() => props.navigation.navigate('List')}>
+        <Text>opacity button</Text>
+    </TouchableOpacity>
+  </View>);
 };
 
 const styles = StyleSheet.create({
